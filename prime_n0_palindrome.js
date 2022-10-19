d5b55059d3b44b2c07d3b94a717de975adf5c756
paras232 <parasmanipatnayak@gmail.com>
2022-10-19 16:34:06 +0530

    added codes to check prime numbers and palindrome.

---
+++ prime_n0_palindrome.js
@@ -0,0 +1,23 @@
+//code for prime no. check!
+let num=57;
+let count=0;
+for(i=1;i<num;i++);
+  if num%i==o {
+    count++
+  }
+
+if(count==2){
+  console.lognum,"is prime";
+}else{
+  condsole.lognum,"is not prime";
+}
+
+//code for palindrome!
+
+let str="padap";
+let flag=0;
+for(i=0;i<str;i++){
+  if(i!=str[str.length-1-i]){
+    flag=1;
+  }
+}
